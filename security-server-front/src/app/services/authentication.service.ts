f7b7b884eb5b648630a13ceedd022e17903d982d
Thibaut Monin <device.thibaut.monin@gmail.com>
2022-12-12 15:37:40 +0100

    Updating authentication service, getToken, setToken, signIn, signUp

--- security-server-front/src/app/services/authentication.service.ts
+++ security-server-front/src/app/services/authentication.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 import { User } from '../models/user.interface';
 
 @Injectable({
@@ -9,22 +8,29 @@ import { User } from '../models/user.interface';
 export class AuthenticationService {
 
   // Todo : Changer la variable locale en constante utilisable globalement
-  private _apiEndpoint: string = 'https://testfuncappgroupedeux.azurewebsites.net/'
+  private _apiEndpoint: string = 'https://testfuncappgroupedeux.azurewebsites.net/api'
 
-  constructor(private _http: HttpClient, public router: Router) { }
+  constructor(private _http: HttpClient) { }
 
-  signIn() {
-
+  signIn(user: User) {
+    return this._http.post(`${this._apiEndpoint}/signin`, user)
+    .subscribe((res: any) => {
+      this.setToken(res.token)
+    })
   }
 
   signUp(user: User) {
-    let api = `${this._apiEndpoint}/signup`
-    
+    let api = `${this._apiEndpoint}/connexion`
+
     return this._http.post(api, user)
   }
 
   getToken() {
+    return localStorage.getItem('access_token')
+  }
 
+  setToken(token: string) {
+    localStorage.setItem('access_token', token)
   }
 
 }
