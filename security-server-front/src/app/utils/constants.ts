b0ec6edbb3bfbf7ed440b1ef2cbafbc3ec8ca7e8
Thibaut Monin <device.thibaut.monin@gmail.com>
2022-12-15 12:54:27 +0100

    Constants update

--- security-server-front/src/app/utils/constants.ts
+++ security-server-front/src/app/utils/constants.ts
@@ -1,6 +1,6 @@
 export default class Constants {
 
-  public static baseUrl: string = "https://testfuncappgroupedeux.azurewebsites.net/api"
-  public static applicationEndpoint: string = 'https://63989490fe03352a94d67844.mockapi.io/application'
+  public static baseUrl: string = "https://testapitristan.azurewebsites.net/api"
+  public static applicationEndpoint: string = 'application'
 
 }
