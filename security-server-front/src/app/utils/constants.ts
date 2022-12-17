95cd1b47e25f56aa937cb90504b7b3fd13c2af78
Thibaut Monin <device.thibaut.monin@gmail.com>
2022-12-17 20:23:14 +0100

    Updating constants with new endpoint

--- security-server-front/src/app/utils/constants.ts
+++ security-server-front/src/app/utils/constants.ts
@@ -1,6 +1,6 @@
 export default class Constants {
 
   public static applicationEndpoint: string = 'application'
-  public static baseUrl: string = "https://testfuncappgroupedeux.azurewebsites.net/api"
+  public static baseUrl: string = "https://test2diiage2.azurewebsites.net/api"
 
 }
