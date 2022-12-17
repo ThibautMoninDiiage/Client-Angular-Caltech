e7861769775878c909558008db26bdfc7129e829
Thibaut Monin <device.thibaut.monin@gmail.com>
2022-12-17 20:23:35 +0100

    Http call on AuthenticationService with signIn method

--- security-server-front/src/app/services/authentication.service.ts
+++ security-server-front/src/app/services/authentication.service.ts
@@ -2,23 +2,27 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.interface';
 import Constants from '../utils/constants';
+import jwtDecode from 'jwt-decode';
+import { map, shareReplay } from 'rxjs';
+import { UserDTO } from '../models/userDTO.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthenticationService {
 
   constructor(private _http: HttpClient) { }
 
-  signIn(user: User) {
-    return this._http.post(`${Constants.baseUrl}/signin`, user)
-    .subscribe((res: any) => {
-      this.setToken(res.token)
-    })
+  signIn(email: string, password: string) {
+    return this._http.post<UserDTO>(`${Constants.baseUrl}/signin`, { mail: email, password: password }).pipe(
+      map(res => this.setToken(res.token)),
+      shareReplay()
+    )
   }
 
   signUp(user: User) {
-    let api = `${Constants.baseUrl}/connexion`
+    let api = `${Constants.baseUrl}/signup`
 
     return this._http.post(api, user)
   }
