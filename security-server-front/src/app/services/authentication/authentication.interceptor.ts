cd6b57786b5d9dfedbebadd9305ba3c1b5b1875b
Thibaut Monin <device.thibaut.monin@gmail.com>
2022-12-18 10:57:09 +0100

    Creating AuthenticationInterceptor

---
+++ security-server-front/src/app/services/authentication/authentication.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthenticationInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    // On récupère le token dans le local storage
+    const token = localStorage.getItem('access_token')
+
+    // Si le token existe, alors on clone et modifie la requête HTTP en passant le token dans le header
+    if (token) {
+      const cloned = request.clone({
+        headers: request.headers.set('Authorization', 'Bearer' + token)
+      })
+
+      return next.handle(cloned)
+    }
+
+    // Sinon on retourne la requête non modifiée
+    return next.handle(request);
+  }
+}
