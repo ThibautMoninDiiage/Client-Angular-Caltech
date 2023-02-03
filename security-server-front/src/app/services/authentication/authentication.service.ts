d1efac669394dd2a0188785ee863ce5d2f7c97e4
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-02-03 13:37:30 +0100

    Pour Tristan

--- security-server-front/src/app/services/authentication/authentication.service.ts
+++ security-server-front/src/app/services/authentication/authentication.service.ts
@@ -1,10 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../../models/user.interface';
 import Constants from '../../utils/constants';
-import jwtDecode from 'jwt-decode';
 import { map, shareReplay } from 'rxjs';
 import { UserDTO } from '../../models/userDTO.model';
+import { Authentication } from 'src/app/models/authentication.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,20 +16,32 @@ export class AuthenticationService {
   constructor(private _http: HttpClient) { }
 
   signIn(email: string, password: string) {
-    return this._http.post<UserDTO>(`${Constants.baseUrl}/signin`, { mail: email, password: password }).pipe(
-      map(res => this.setToken(res.token)),
-      shareReplay()
+    var url = ""
+    return this._http.post<Authentication>(`${Constants.baseUrl}/signin`, { mail: email, password: password, url: url }).pipe(
+      map(res => {
+        window.location.href = res.urlGrant
+        this.getUserToken(res.urlGrant)
+      }), shareReplay()
+    )
+  }
+
+  getUserToken(codeGrant: string) {
+    const queryString = window.location.search
+    console.log(queryString)
+    
+    return this._http.post<UserDTO>(`${Constants.baseUrl}/AuthenticateGrant`, { codeGrant: "" }).pipe(
+      map(res => {
+        this.setToken(res.token)
+      }), shareReplay()
     )
   }
 
   signUp(user: User) {
-    let api = `${Constants.baseUrl}/signup`
-
-    return this._http.post(api, user)
+    return this._http.post(`${Constants.baseUrl}/signup`, user)
   }
 
-  getToken() {
-    return localStorage.getItem('access_token')
+  getToken(): string {
+    return JSON.parse(localStorage.getItem('access_token')!)
   }
 
   setToken(token: string) {
